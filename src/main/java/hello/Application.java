c02c376001456614399a2dad9a0a60e04b0ca8d9
Parijat Mishra <parijat.mishra@gmail.com>
2019-08-11 14:22:09 +0800

    Remove listing of spring boot beans from startup output.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -18,12 +18,6 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Beans provided by Spring Boot:");
-            final String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanDefinitionNames);
-            for (String name : beanDefinitionNames) {
-                System.out.println(name);
-            }
         };
     }
 }
